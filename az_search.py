ca1118575bbf2bcdb96ded7490222dbd1aef6ded
Aditya Pandey <addherbs@gmail.com>
2017-07-11 01:21:28 -0500

    final az lyrics searching

--- az_search.py
+++ az_search.py
@@ -1,48 +1,77 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-
-name = 'crazy train'
-def generate_az_url():
+# name = 'crazy train'
+def generate_az_url(song_name):
     url = 'http://search.azlyrics.com/search.php?q='
-    final_name = ''
-    split = name.split(' ')
-    print(split)
-    final_name = ''
+    split = song_name.split(' ')
     lenght = len(split)
-    print(lenght)
     count = 1
+    generated_url = ''
     for word in split:
-        print (count)
         if count==lenght:
-            final_name = final_name + word
+            generated_url = generated_url + word
         else:
-            final_name = final_name  + word + '+'
+            generated_url = generated_url  + word + '+'
         count = count + 1
-    final_name = url + final_name
-    return final_name
+    generated_url = url + generated_url
 
-final_url = generate_az_url()
-print(final_url)
+    list_of_song_names, result_dictionary = changeInClass (generated_url)
+    return list_of_song_names, result_dictionary
+
+# final_url = generate_az_url()
+
+def changeInClass(final_url):
+    source = urllib.request.urlopen (final_url).read ()
+    soup = BeautifulSoup (source, 'lxml')
+    result_dictionary = {}
+    count = 0
+    for each_row in soup.find_all('td', class_='text-left visitedlyr'):
+        count = count + 1
+        if count ==6:
+            break
+        else:
+            names = [names.text for names in each_row.find_all('b')]
+            # print(names)
+            link = each_row.find('a').get('href')
+            artist_name = names[1]
+            song_name = names[0]
+            entire_song_name = str (song_name + ' By Artist ' + artist_name)
+            # print(each_row.find('a').get('href'))
+            # print('song ', names[0])
+            # print ('artist ', names[1])
+            # print(entire_song_name , ' ' , link)
+            result_dictionary[entire_song_name] = link
+
+    list_of_song_names = result_dictionary.keys()
+    # print(list_of_song_names)
+    return list_of_song_names, result_dictionary
+    # for k,v in result_dictionary.items():
+    #     print('key: ' , k ,' ==>' , 'value: ', v)
+
+# changeInClass(final_url)
 
 
 def scrape_az_for_links(final_url):
     source = urllib.request.urlopen (final_url).read ()
     soup = BeautifulSoup (source, 'lxml')
     list = []
-    # print(soup.prettify())
     table = soup.table
-    for anc in table.find_all('a'):
-        # print(anc.text)
-        title = anc.text.lower()
-        if (name.lower() == title):
-            list.append(anc.get('href'))
+    count = 0
+    for anc in table.find_all ('a'):
 
-        # print(anc.get('href'))
-    # print(table)
+        # print(anc)
+        for b in anc.find_all('b'):
+            print("b tags: ", b)
+        title = anc.text.lower ()
+        if (name.lower () == title):
+            list.append (anc.get ('href'))
+            print(anc.get('href'))
+        print('------------------')
+# print(table)
     return list
 
+# az_search_result_links = scrape_az_for_links(final_url)
 
-az_search_result_links = scrape_az_for_links(final_url)
-print('------------------')
-print(az_search_result_links)
+# print('------------------')
+# print(az_search_result_links)
