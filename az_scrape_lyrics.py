7d1eeed970f054bbd0baf56cfcb2ed569904e2ed
Aditya Pandey <addherbs@gmail.com>
2017-07-11 01:21:19 -0500

    modified az scraping

--- az_scrape_lyrics.py
+++ az_scrape_lyrics.py
@@ -1,21 +1,20 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-google_url = 'https://www.google.com/#q=craazy+train+lyrics'
-url = 'http://www.azlyrics.com/lyrics/ozzyosbourne/crazytrain.html'
-song_name = input('Enter the Song Name:')
+# google_url = 'https://www.google.com/#q=craazy+train+lyrics'
 
+def scrape_lyrics(url):
+    # url = 'http://www.azlyrics.com/lyrics/ozzyosbourne/crazytrain.html'
+    source = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(source, 'lxml')
 
+    # print(soup.prettify())
+    count =0
+    for out in soup.find_all('div' , class_="container main-page"):
+        for inn in out.find_all ('div'):
+            count = count +1
+            if count == 11:
+                song_lyrics = str(inn.text).replace('<br>','\n').split('\n')
 
-source = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(source, 'lxml')
-
-# print(soup.prettify())
-count =0
-for out in soup.find_all('div' , class_="container main-page"):
-    for inn in out.find_all ('div'):
-        count = count +1
-        if count == 11:
-            song_lyrics = inn.text
-
-print(song_lyrics)
+    print(song_lyrics)
+    return song_lyrics
