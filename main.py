e37215b1b9e1a45707bd3b8a13bb1e7d46a05788
Aditya Pandey <addherbs@gmail.com>
2017-07-11 01:21:33 -0500

    changes

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from az_scrape_lyrics import scrape_lyrics
+from az_search import generate_az_url, changeInClass
 
 app = Flask(__name__)
+list_of_song_names = []
+result_dictionary = {}
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Enter the song name to find the lyrics associated with it'
+    global list_of_song_names,result_dictionary
+    if request.method == 'GET':
+        list_of_song_names = []
+        return render_template('index.html' , name = '', list_of_song_names = list_of_song_names)
+    elif request.method == 'POST':
+        name = request.form.get('song')
+        # print(name)
+        list_of_song_names, result_dictionary = generate_az_url(name)
+
+        return render_template('index.html', name = name, list_of_song_names = list_of_song_names )
+
+@app.route('/giveSongResult', methods=['GET', 'POST'])
+def showLyrics():
+    if request.method == 'GET':
+        list_of_song_names = []
+        return render_template('index.html' , name = '', list_of_song_names = list_of_song_names)
+    elif request.method == 'POST':
+        list_of_song_names = []
+        key = request.form['songname']
+        print('key: ',key)
+        link_value = result_dictionary[key]
+        print(link_value)
+
+        lyrics = scrape_lyrics(link_value)
+        lyrics_array = []
+
+        for lines in lyrics:
+            lyrics_array.append(lines)
+        return render_template('song_lyrics.html' , song_lyrics = lyrics_array , song_name = key)
 
 if __name__ == "__main__" :
     app.run()
