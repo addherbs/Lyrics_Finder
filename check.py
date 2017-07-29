f7a111b513eb575a6c26f7bedf3590f2e2e66370
Aditya Pandey <addherbs@gmail.com>
2017-07-28 23:10:57 -0500

    checking lyricsed.com

--- check.py
+++ check.py
@@ -1,21 +1,16 @@
 
 from bs4 import BeautifulSoup
 import urllib.request
-from google import search
+# from google import search
 from urllib.request import Request, urlopen
 
 url = 'http://www.lyricsted.com/search/?s=wajah+tum+ho'
 
-req = Request (url, headers={'User-Agent': 'Mozilla/5.0'})
-source = urlopen(req).read()
+source = urllib.request.urlopen (url).read ()
 soup = BeautifulSoup (source, 'lxml')
 
-for links in search('https://www.google.com/#q=lyricsmint.com+wajah+tum+ho+lyrics'):
-    print(links)
-
-
 # print(soup.prettify())
-# print(soup.find_all('a'))
-#
-# for anc in soup.find_all('a'):
-#     print(anc)
+count = 0
+for out in soup.find_all ('div', class_='gsc-wrapper'):
+    for a in out.find_all('a'):
+        print(a)
